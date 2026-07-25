97c5c19421910cf30442e3c11d9933b5fc79bb11
CarloFidel <carloftaboada@gmail.com>
2026-07-25 19:55:55 +0200

    feat: update background styles and add theme variables

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="justify-center items-center flex flex-col min-h-screen bg-linear-to-b from-[#2e026d] to-[#15162c] text-white">
+    <div className="justify-center items-center flex flex-col min-h-screen bg-background text-white">
       <p>Hello Vite + React!</p>
       <button onClick={() => setCount(count + 1)}>Count is: {count}</button>
     </div>
